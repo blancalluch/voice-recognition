f36da552a742aafc89bae6ac18993c37df145675
Blanca Lluch <lluchponceblanca@gmail.com>
2019-12-16 21:55:20 +0100

    nncode

--- predictPrep.py
+++ predictPrep.py
@@ -4,21 +4,17 @@ import creatingDataSet as cds
 import json
 from keras import models
 from keras.models import model_from_json
+import neuralN as nn
 
 def prediction(arr):
 
-    with open('./models/Model_0.6007194519042969_16-20-44.h5.json','r') as f:
-        model_json = json.load(f)
-
-    model = model_from_json(model_json)
-    model.load_weights('./models/Model_0.6007194519042969_16-20-44.h5.h5')
-    print('Model loaded')
-    X_test=np.hstack(arr)
+    X_test=np.vstack(arr)
     #X_test=np.expand_dims(arr,axis=0)
     print(X_test.shape,arr)
     #clf = load('./models/primer_nn.joblib')
     #res = clf.predict(X_test)
-    res = model.predict(X_test)
+    res=nn.nn_model(X_test)
+    #res = model.predict(X_test)
     return res
 
 def treating(data):
